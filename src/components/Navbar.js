7e55a08a34d5440023ea5d5edc826d84988bebde
ericveganoguera <eveno229@gmail.com>
2023-04-13 12:25:36 +0200

    Create navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,41 @@
-import { NavLink } from "react-router-dom"
-function Navbar(){
-    return(
-    <nav>
-        <NavLink to="/">Home  |  </NavLink>
-        <NavLink to="/muscles">Muscles  |  </NavLink>
-        <NavLink to="/exercises">Exercises  |  </NavLink>
-        <NavLink to="/workouts">Workout</NavLink>
-    </nav>
-    )
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { LinkContainer } from "react-router-bootstrap";
+
+function NavbarF() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand>Fitness App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/muscles">
+              <Nav.Link>Muscles</Nav.Link>
+            </LinkContainer>
+            <NavDropdown title="Exercises" id="basic-nav-dropdown">
+              <LinkContainer to="/exercises">
+                <NavDropdown.Item>List</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/exercises/create"><NavDropdown.Item>Create</NavDropdown.Item></LinkContainer>
+              <NavDropdown.Divider />
+              <LinkContainer to="/exercises/my"><NavDropdown.Item>My Exercises</NavDropdown.Item></LinkContainer>
+            </NavDropdown>
+            <NavDropdown title="Workouts">
+            <LinkContainer to="/workouts"><NavDropdown.Item>List</NavDropdown.Item></LinkContainer>
+            <LinkContainer to="/workouts/create"><NavDropdown.Item>Create</NavDropdown.Item></LinkContainer>
+              <NavDropdown.Divider />
+              <LinkContainer to="/workouts/my"><NavDropdown.Item>My Workouts</NavDropdown.Item></LinkContainer>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
-export default Navbar
+export default NavbarF;
