5698bd4c22976ac3fa58316839f92db102d60ddf
Alex Uceda <alexucedacintas@gmail.com>
2023-04-13 17:42:26 +0200

    add muscle list

--- src/pages/Muscles.js
+++ src/pages/Muscles.js
@@ -1,6 +1,35 @@
-function Muscles(){
-    return (
+import axios from "axios";
+import { useState, useEffect } from "react";
+import MuscleCard from "../components/MuscleCard";
+
+function Muscles() {
+  const [muscles, setMuscles] = useState(null);
+
+  const getAllMuscles = () => {
+    axios
+      .get(
+        `${process.env.REACT_APP_API_URL}/muscles` ||
+          "http://127.0.0.1:5005/api/muscles"
+      )
+      .then((response) => {
+        setMuscles(response.data);
+      });
+  };
+  useEffect(() => {
+    getAllMuscles();
+  }, []);
+  return (
+    <>
         <h1>Muscles</h1>
-    )
-    }
-    export default Muscles
+    {muscles ?  
+        muscles.map((muscle) => {
+        return (
+          <MuscleCard key={muscle._id} {...muscle}/>
+        )})
+      : "Loading..."
+      }
+      
+    </>
+  );
+}
+export default Muscles;
