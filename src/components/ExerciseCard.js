b812c57c512301de850b3a9a5da4c115b060b425
ericveganoguera <eveno229@gmail.com>
2023-04-13 13:53:23 +0200

    Add Exercises list

---
+++ src/components/ExerciseCard.js
@@ -0,0 +1,27 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+function ExerciseCard({
+  name,
+  type,
+  muscle,
+  equipment,
+  difficulty,
+  instructions,
+}) {
+  return (
+    <Card style={{ width: "20rem", height:"30rem" ,padding:"20px",margin:"5px", display: "inline-block", overflow:"scroll"}}>
+      <Card.Img
+        variant="top"
+        src="https://static.strengthlevel.com/images/illustrations/incline-hammer-curl-1000x1000.jpg"
+      />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{instructions}</Card.Text>
+        <Button variant="danger">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ExerciseCard;
--- src/pages/Exercises.js
+++ src/pages/Exercises.js
@@ -1,6 +1,41 @@
-function Exercises(){
-return (
-    <h1>Exercises</h1>
-)
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ExerciseCard from "../components/ExerciseCard";
+
+function Exercises() {
+  const [exercises, setExercises] = useState(null);
+  const imageLoading = "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921"
+
+  const getAllExercises = () => {
+    axios
+      .get(
+        `${process.env.REACT_APP_API_URL}/exercises` ||
+          "http://localhost:5005/api/exercises"
+      )
+      .then((response) => {
+        setExercises(response.data);
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+      });
+  };
+  useEffect(() => {
+    getAllExercises();
+  }, []);
+  return (
+    <>
+      <h1>Exercises!</h1>
+      {exercises ? (
+        exercises.map((exercise) => (
+          <ExerciseCard key={exercise._id} {...exercise} />
+        ))
+      ) : (
+        <img
+          src={imageLoading}
+          alt="Loading"
+        />
+      )}
+    </>
+  );
 }
-export default Exercises
+export default Exercises;
