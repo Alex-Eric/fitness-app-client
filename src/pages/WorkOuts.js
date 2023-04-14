46c7d43addb1bf2861e76de369e748f9eb38deed
Alex Uceda <alexucedacintas@gmail.com>
2023-04-14 13:46:55 +0200

    create basic workouts

--- src/pages/WorkOuts.js
+++ src/pages/WorkOuts.js
@@ -1,6 +1,28 @@
-function Workouts(){
-    return (
-        <h1>Workouts</h1>
-    )
-    }
-    export default Workouts
+import axios from "axios";
+import { useEffect, useState } from "react";
+import {Spinner} from "react-bootstrap"
+
+function Workouts() {
+    const [workouts, setWorkouts] = useState(null)
+
+function getAllWorkouts(){
+    axios.get(`${process.env.REACT_APP_API_URL}/workouts`)
+    .then((response)=>{
+      setWorkouts(response.data)
+    });
+}
+useEffect(()=>{
+    getAllWorkouts();
+  },[])
+  return (
+    <>
+      <h1>Workouts</h1>
+      {workouts ? 
+      workouts.map((element)=><h1>{element.name}</h1>)
+      :
+      <Spinner animation="border"/>
+      }
+    </>
+  );
+}
+export default Workouts;
