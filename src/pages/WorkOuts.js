129360c5a46b3d1950d5bd08a00e7b99b38b9a7d
Alex Uceda <alexucedacintas@gmail.com>
2023-04-18 00:32:25 +0200

    navbar workout & Edit component

--- src/pages/WorkOuts.js
+++ src/pages/WorkOuts.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Spinner } from "react-bootstrap";
+import { Button, Spinner } from "react-bootstrap";
 import WorkoutAccordion from "../components/WorkoutAccordion";
 import Accordion from "react-bootstrap/Accordion";
+import { Link } from "react-router-dom";
 function Workouts() {
   const [workouts, setWorkouts] = useState(null);
   const [deleteCheck, setDeleteCheck] = useState(false);
+  const [editCheck, setEditCheck] = useState(false)
   function getAllWorkouts() {
     axios.get(`${process.env.REACT_APP_API_URL}/workouts`).then((response) => {
       setWorkouts(response.data);
@@ -14,10 +16,12 @@ function Workouts() {
   useEffect(() => {
     getAllWorkouts();
     setDeleteCheck(false);
-  }, [deleteCheck]);
+    setEditCheck(false)
+  }, [deleteCheck, editCheck]);
   return (
     <div style={{ margin: " 0 20%" }}>
       <h1>Workouts</h1>
+      <Link to="/workouts/create">Create</Link>
       <Accordion defaultActiveKey="0">
         {workouts ? (
           workouts.map((workout) => {
@@ -25,6 +29,7 @@ function Workouts() {
               <WorkoutAccordion
                 workout={workout}
                 setDeleteCheckcallback={setDeleteCheck}
+                setEditCheckcallback={setEditCheck}
               />
             );
           })
