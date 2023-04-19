4819bef13bc6ac38970c94329fb435f6783aa8db
ericveganoguera <eveno229@gmail.com>
2023-04-19 17:49:39 +0200

    Add Homepage styles

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,104 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import MuscleGroupImage from "../components/MuscleGroupImage.tsx";
+import { useContext } from "react";
+import { Button, Carousel } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
 
 function HomePage() {
-  
+  const { isLoggedIn } = useContext(AuthContext);
+
   return (
-    <>
-      <h1>HomePage</h1>
+    <div>
+      {!isLoggedIn && (
+        <>
+          <section className="banner">
+            <h2>Find your ideal exercise routine!</h2>
+            <Link to="/register">
+              <Button variant="danger"> Sign up for free!</Button>
+            </Link>
+          </section>
+        </>
+      )}
+      <section className="introduction">
+        <h2>Welcome to Fitness App</h2>
+        <p>
+          Our website is a tool to help you maintain a healthy lifestyle through
+          exercise and workouts!
+        </p>
+      </section>
+      <Carousel variant="dark">
+        <Carousel.Item style={{ height: "500px" }}>
+          <Link to="/muscles">
+            <img
+              className="d-block w-100"
+              src="https://www.incimages.com/uploaded_files/image/1024x576/getty_163226029_970551970450061_44546.jpg"
+              alt="First slide"
+            />
+          </Link>
+          <Carousel.Caption style={{ color: "white" }}>
+            <h5>Muscles!</h5>
+            <p>
+              You can see all the muscles with images that you can exercise on
+              our website!
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item style={{ height: "500px" }}>
+          <Link to="/exercises">
+            <img
+              className="d-block w-100"
+              src="https://www.samedaysupplements.com/blog/wp-content/uploads/2018/06/leg-day-workout-banner.jpg"
+              alt="Second slide"
+            />
+          </Link>
+          <Carousel.Caption style={{ color: "white" }}>
+            <h5>Exercises!</h5>
+            <p>
+              Here you will find a series of basic exercises in addition to the
+              exercises created by the community
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item style={{ height: "500px" }}>
+          <Link to="/workouts">
+            <img
+              className="d-block w-100"
+              src="https://www.chelseapiersct.com/cpct2015/cache/file/3CB638CD-ECC2-55B3-79E56E1285465BBC_fitness-banner.jpg"
+              alt="Third slide"
+              style={{ height: "500px", width: "auto" }}
+            />
+          </Link>
+          <Carousel.Caption style={{ color: "white" }}>
+            <h5>Workouts!</h5>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
 
+      {/* Sección de características */}
+      <section className="features">
+        <h2>Fitness App Features</h2>
+        <ul>
+          <li>Create and save your custom workout routines</li>
+          <li>View exercises with muscle images and detailed descriptions</li>
+          <li>Progress Log</li>
+        </ul>
+      </section>
 
-    </>
+      {/* Llamado a la acción */}
+      {!isLoggedIn && (
+        <>
+          <section className="cta">
+            <h2>Sign up to access all features and services exclusive</h2>
+            <Link to="/register">
+              <Button variant="danger"> Sign up for free!</Button>
+            </Link>
+          </section>
+        </>
+      )}
+    </div>
   );
-    }
+}
 
 export default HomePage;
